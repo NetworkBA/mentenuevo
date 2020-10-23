dc0810ff1970f9140e657cc384cc272014d1990c
juarezfdo <juarezfdo96@gmail.com>
2020-10-22 20:54:36 -0500

    Commit inicial

---
+++ resources/views/front/index.blade.php
@@ -0,0 +1,77 @@
+@extends('layouts.front')
+
+@section('body')
+
+	<div style="background-color: #F0F0F1; padding-top: 80px;padding-bottom: 80px;">
+		<div class="container">
+			<div class="row justify-content-between">
+				<div class="col-lg-6" style="padding-bottom: 20px;">
+					<div class="encabezado">
+						<h1>Todo lo</h1>
+						<h1 style="margin-top: -15px;">que sientes</h1>
+						<h1 style="color: #F5B530;margin-top: -15px;">es real.</h1>
+					</div>
+					<p><b>Es muy importante tratar la salud mental. En mente 4.0 tenemos un plan que se acomoda a tus necesidades, con profesionales que resolverán todas tus dudas, todo desde la comodidad de tu hogar.</b></p>
+					<p>Todos nosotros, en determinados momentos de nuestras vidas, necesitamos tener asesoramiento y recibir la ayuda de otras personas.</p>
+					<a class="btn" href="#" style="background-color: #F47E6E;color:white;" role="button">Conoce Más</a>
+				</div>
+				<div class="col-lg-6">
+					<img src="images/img1.png" class="img-fluid">
+				</div>
+			</div>	
+		</div>	
+	</div>
+
+	<div style="padding-top: 80px;padding-bottom: 80px;">
+		<div class="container">
+			<div class="row justify-content-between">
+				<div class="col-lg-6">
+					<img src="images/img2.png" class="img-fluid">
+				</div>
+				<div class="col-lg-6" style="padding-bottom: 20px;">
+					<div class="encabezado">
+						<h1>Tu tienes el poder</h1>
+						<h1 style="margin-top: -15px;">en la terapia</h1>
+					</div>
+					<p><b>Es cuestión de que le pierdas el miedo y entiendas que es por tu bien, el cerebro es un órgano que hay que ejercitar y también cuidar.</b></p>
+					<p>Trabajemos en la salud mental, nuestra mente al igual que todo nuestro cuerpo necesita atención. Más hoy en día que vivimos algo único y que nos está cambiando la vida. </p>
+					<br>
+					<h5 style="text-align: right; color: #F5B530;"><b>Piensa, Experimenta y Aprende.</b></h5>
+				</div>
+			</div>	
+		</div>	
+	</div>
+
+	<div style="background-color: #F0F0F1; padding-top: 80px;padding-bottom: 80px;">
+		<div class="container">
+			<h1 style="text-align: center;color: #77D0E8">
+				Testimonios
+			</h1>
+			<br>
+			<br>
+			<br>
+			<div class="row justify-content-between">
+				<div class="col-lg-3" style="background-color: white;padding: 20px;">
+					<h5 style="text-align: center">Eugenia Salazar</h5>
+					<p style="text-align: center;"><i style="color:#27CC9A" class="fa fa-2x fa-quote-left"> </i></p>
+					<p style="text-align: center"><i>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua."</i></p>
+				</div>
+				<div class="col-lg-3" style="background-color: white;padding: 20px;">
+					<h5 style="text-align: center">Eugenia Salazar</h5>
+					<p style="text-align: center;"><i style="color:#27CC9A" class="fa fa-2x fa-quote-left"> </i></p>
+					<p style="text-align: center"><i>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua."</i></p>
+				</div>
+				<div class="col-lg-3" style="background-color: white;padding: 20px;">
+					<h5 style="text-align: center">Eugenia Salazar</h5>
+					<p style="text-align: center;"><i style="color:#27CC9A" class="fa fa-2x fa-quote-left"> </i></p>
+					<p style="text-align: center"><i>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua."</i></p>
+				</div>
+			</div>	
+		</div>	
+	</div>
+	
+
+@endsection
